20fb019cfaeedb66033be8cd7732c1d0138d2e1e
lyn <dldudsh83@gmail.com>
2023-02-06 23:37:25 +0900

    set Redux

--- src/components/views/LandingPage.js
+++ src/components/views/LandingPage.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 
 function LandingPage() {
+  const isAuth = useSelector((state) => state.user.isAuth);
+  console.log(isAuth);
   return (
     <div>
       <Outlet />
